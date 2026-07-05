db05501175f341943366b0e19afbe0332a7dd6c9
alamsamir7666-ux <alamsamir7666@gmail.com>
2026-07-05 10:22:04 +0600

    Fix missing imports for url_launcher and cached_network_image in home_screen

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../core/models/misc.dart';
